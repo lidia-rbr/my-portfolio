bfc581aac0cf15527c973b4c2c7b8a3e7cb9d1ad
Lidia <lidia.calicia@gmail.com>
2024-07-08 06:15:44 +0200

    fix

--- src/Components/Projects/ProjectsIndex.jsx
+++ src/Components/Projects/ProjectsIndex.jsx
@@ -43,7 +43,7 @@ const Card = styled.div`
   flex: 0 0 40.3%;
   display: flex;
   flex-direction: column;
-  max-height: 540px;
+  max-height: 570px;
 
   @media (max-width: 769px) {
     width: 100%;
